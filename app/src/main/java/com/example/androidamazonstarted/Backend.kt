73c43fb34ea441bd6d29fd367e8a33d61a9b09c4
chenzhizs <chenzhi@qq.com>
2022-06-30 15:58:49 +0900

    3.2.4在运行时配置Amplify身份验证库

--- app/src/main/java/com/example/androidamazonstarted/Backend.kt
+++ app/src/main/java/com/example/androidamazonstarted/Backend.kt
@@ -9,6 +9,7 @@ import android.util.Log
 
 
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 
 
 import com.amplifyframework.core.Amplify
@@ -26,7 +27,13 @@ object Backend {
         try {
 
 
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
+
+
             Amplify.configure(applicationContext)
+
+
+
 
 
             Log.i(TAG, "Initialized Amplify")
