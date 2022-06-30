582e646c515d856e1e74b1ef272590dd7613ac2d
chenzhizs <chenzhi@qq.com>
2022-06-30 15:46:43 +0900

    2.2.5在运行时初始化Amplify

---
+++ app/src/main/java/com/example/androidamazonstarted/Application.kt
@@ -0,0 +1,29 @@
+package com.example.androidamazonstarted
+
+
+
+
+
+import android.app.Application
+
+
+
+
+
+class AndroidGettingStartedApplication : Application() {
+
+    override fun onCreate() {
+
+
+        super.onCreate()
+
+        // initialize Amplify when application is starting
+
+
+        Backend.initialize(applicationContext)
+
+
+    }
+
+
+}
